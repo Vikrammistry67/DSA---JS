1d63c1412e9374c5746b4b548700b304bb777bbe
Vikram Mistry <mistryv414@gmail.com>
2026-09-10 17:56:43 +0530

    Add Student constructor and update User instance creation examples

--- OOPS/oops.js
+++ OOPS/oops.js
@@ -1,4 +1,5 @@
 // ------------------------------------------------------------------- OOPS ----------------------------------------------------------------
+function Student() { };
 
 function User(username, age, isLoggedIn) {
     this.username = username;
@@ -11,8 +12,20 @@ function User(username, age, isLoggedIn) {
     return this;
 };
 
+
+
 let userOne = new User('Vikram Mistry', 23, true);
 let userTwo = new User('Kuhu Sharma', 24, false);
+let userThree = new Student();
+// console.log(userOne.greeting());
+// console.log(userTwo.greeting());
 
-console.log(userOne.greeting());
-console.log(userTwo.greeting());
+// console.log(userOne instanceof User);
+// console.log(userThree instanceof User);
+
+// console.log(userThree instanceof User);
+// console.log(userThree instanceof Object);
+
+
+// console.log(userOne.greeting() instanceof User);
+console.log(userOne.username instanceof User);
