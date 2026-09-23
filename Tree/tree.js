d7f8b3b63201c9862c3036f5d53f4e1b4ae3c545
Vikram Mistry <mistryv414@gmail.com>
2026-09-23 19:15:56 +0530

    Refactor tree implementation to improve buildTree method and add traversal methods

--- Tree/tree.js
+++ Tree/tree.js
@@ -1,5 +1,3 @@
-let prompt = require('prompt-sync')();
-
 class Node {
     constructor(val) {
         this.val = val;
@@ -9,26 +7,66 @@ class Node {
 };
 
 
-
+let prompt = require('prompt-sync')();
 class Tree {
     buildTree() {
-        let data = prompt('Enter data');
+        let data = prompt('Enter value ');
         if (data == -1) return null;
 
         let root = new Node(data);
-        console.log('Enter left node of ', root);
-
+        console.log('Enter left node  of : ', data);
         root.left = this.buildTree();
-        console.log('Enter right node of ', root);
 
+        console.log('Enter right node of : ', data);
         root.right = this.buildTree();
 
+
         return root;
+    };
+
+
+    // Data -> left -> right
+    preOrder(root) {
+        if (root == null) return;
+        process.stdout.write(root.val + ' '); //data
+        this.preOrder(root.left); //left
+        this.preOrder(root.right); //right
+    };
+
+
+    // left -> right -> data.
+    postOrder(root) {
+        if (root == null) return;
+
+        this.postOrder(root.left);
+        this.postOrder(root.right);
+        process.stdout.write(root.val + ' ');
 
     };
+
+
+    // left -> data -> right.
+    inOrder(root) {
+        if (root == null) return;
+
+        this.inOrder(root.left);
+        process.stdout.write(root.val + ' ');
+        this.inOrder(root.right);
+
+    };
+
 };
 
 
+
 let obj = new Tree();
-obj.buildTree();
-console.log(obj);
+let root = obj.buildTree();
+
+console.log('preOrder traversal -----> ')
+obj.preOrder(root);
+
+console.log('postOrder traversal -----> ')
+obj.postOrder(root);
+
+console.log('inOrder traversal -----> ')
+obj.inOrder(root);
